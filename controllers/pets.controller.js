97905104ecc2ac17ce21bdbdfbf7e9647c4edeb4
joselrdg <josesietepicos@gmail.com>
2021-04-17 02:06:09 +0200

    get pet

--- controllers/pets.controller.js
+++ controllers/pets.controller.js
@@ -1,33 +1,36 @@
 const createError = require("http-errors");
 const Pet = require("../models/Pet.model");
 
-
 module.exports.list = (req, res, next) => {
-    Pet.find()
-      .then(pets => {
-        if (!pets) {
-          next(createError(404, 'Pet not found'))
-        } else {
-          res.json(pets)
-        }
-      })
-      .catch(next)
-  }
-  
-module.exports.get = (req, res, next) => {
-    Pet.findById(req.params.id)
-      .then(pet => {
-        if (!pet) {
-          next(createError(404, 'Pet not found'))
-        } else {
-          res.json(pet)
-        }
-      })
-      .catch(next)
-  }
+  Pet.find()
+    .then((pets) => {
+      if (!pets) {
+        next(createError(404, "Pet not found"));
+      } else {
+        res.json(pets);
+      }
+    })
+    .catch(next);
+};
 
-  module.exports.create = (req, res, next) => {
-    Pet.create(req.body)
-      .then(pet => res.status(201).json(pet))
-      .catch(next)
+module.exports.get = (req, res, next) => {
+  if (!req.body === {}) {
+    console.log(req);
+    console.log("hola");
+    Pet.find(req.body)
+      .then((pet) => {
+        if (!pet) {
+          next(createError(404, "Pet not found"));
+        } else {
+          res.json(pet);
+        }
+      })
+      .catch(next);
   }
+};
+
+module.exports.create = (req, res, next) => {
+  Pet.create(req.body)
+    .then((pet) => res.status(201).json(pet))
+    .catch(next);
+};
