46c2a898b7ffb165d890518a4661cb92161d59cc
joselrdg <josesietepicos@gmail.com>
2021-04-30 16:52:35 +0200

    date

--- controllers/pets.controller.js
+++ controllers/pets.controller.js
@@ -12,15 +12,15 @@ module.exports.getAll = (req, res, next) => {
     })
     .catch(next);
 };
-module.exports.get = (req, res, next) => { 
-  const {id } = req.params
-  console.log(id)
+module.exports.get = (req, res, next) => {
+  const { id } = req.params;
+  console.log(id);
   Pet.findById(id)
     .then((pet) => {
       if (!pet) {
         next(createError(404, "Pet not found"));
       } else {
-        console.log(pet)
+        console.log(pet);
         res.json(pet);
       }
     })
@@ -34,7 +34,7 @@ module.exports.list = (req, res, next) => {
       if (!pets) {
         next(createError(404, "Pet not found"));
       } else {
-        console.log('Mascotas encontradas');
+        console.log("Mascotas encontradas");
         res.json(pets);
       }
     })
@@ -42,20 +42,40 @@ module.exports.list = (req, res, next) => {
 };
 
 module.exports.create = (req, res, next) => {
-  if (req.body.user === req.currentUser)
-  {Pet.create(req.body)
-    .then((pet) => {console.log('Mascota creada'); res.status(201).json(pet)})
-    .catch(next);}
+  if (req.body.user === req.currentUser) {
+    Pet.create(req.body)
+      .then((pet) => {
+        console.log("Created pet");
+        res.status(201).json(pet);
+      })
+      .catch(next);
+  }
 };
 
-
 module.exports.editPetUser = (req, res, next) => {
-  console.log(req.body)
-  const id = req.params.user
-  Pet.findByIdAndUpdate(id, req.body,{new:true})
-      .then((p) => {
-        console.log(p)
-          console.log('Existe----------------------')
-      })
-      .catch((e) => next(e));
-}
+  console.log(req.params);
+  const id = req.params.id;
+  Pet.findByIdAndUpdate(id, { $set: req.body }, { new: true })
+    .then((p) => {
+      console.log("Existe----------------------");
+      if (p === null) {
+        console.log('null');
+        next(createError(404, "the pet could not be updated"));
+      } else {
+        console.log(id);
+        Pet.findById(id).then((pet) => {
+          console.log(pet);
+          if (!pet) {
+            console.log('No encontrado')
+            next(createError(404, "Pet not found"));
+          } else {            
+            res.json(pet);
+          }
+        });
+      }
+    })
+    .catch((e) => {
+      console.log("error actualizar");
+      next(e);
+    });
+};
